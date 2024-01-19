0f1b2b4b227180da7281582b823c6da348c4bdd2
alexcl01 <acastellanol1@alumnos.nebrija.es>
2024-01-19 17:57:44 +0100

    Ordinaria

--- gql/schema.ts
+++ gql/schema.ts
@@ -9,13 +9,12 @@ export const typeDefs = `#graphql
 
     type Query {
         contacts: [Contact!]!
-        contact: (id: ID!): Contact!
+        contact(id: ID!): Contact!
     }
 
     type Mutation {
         addContact(nameAndLastNames: String!, phoneNumber: String!): Contact!
         getContact(id: ID!): Contact!
-        getContacts: [Contact!]!
         deleteContact(id: ID!): Boolean!
         updateContact(id: ID!, nameAndLastNames: String!, phoneNumber: String!): Contact!
     }
