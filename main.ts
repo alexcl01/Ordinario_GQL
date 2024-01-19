1c67edd3e5b78897b8c0ceeaa5a74338ad58ca35
alexcl01 <acastellanol1@alumnos.nebrija.es>
2024-01-19 17:33:32 +0100

    Ordinaria

--- main.ts
+++ main.ts
@@ -1,20 +1,11 @@
 import {ApolloServer} from "npm:@apollo/server@4.9.5";
 import {startStandaloneServer} from "npm:@apollo/server@4.9.5/standalone";
-import mongoose from "npm:mongoose@8.0.1";
 import { typeDefs } from "./gql/schema.ts";
 import { Contact } from "./resolvers/Contact.ts";
 import { Mutation } from "./resolvers/mutation.ts";
 import { Query } from "./resolvers/query.ts";
 
-const MONGO_URL = "mongodb+srv://acastellanol1:Acl650537271@clusterquinto.qmjbzqt.mongodb.net/OrdinariaGQL";
-if (!MONGO_URL) {
-  throw new Error("Please provide a MongoDB connection string");
-}
 
-// Connect to MongoDB
-await mongoose.connect(MONGO_URL);
-
-console.info("🚀 Connected to MongoDB");
 
 const server = new ApolloServer({
   typeDefs,
